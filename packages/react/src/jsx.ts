5d94eee40fcfa86f91fbf751dd0ee6b5b1bd25b6
884153724@qq.com <570588655@qq.com>
2023-06-06 23:18:27 +0800

    feat: add link react-demo

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -68,4 +68,31 @@ export const jsx = function (
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDev = jsx;
+export const jsxDEV = function (type: ElementType, config: any) {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
---
+++ react-demo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const jsx = <div><span>hello kreact</span></div>
+
+
+console.log(jsx)
+console.log(React)
