b0c5029ddc36d33c311a32d50fc1afd6124835c9
884153724@qq.com <570588655@qq.com>
2023-06-19 23:21:47 +0800

    feat: add update & updateQueue

--- packages/reconciler/src/completeWork.ts
+++ packages/reconciler/src/completeWork.ts
@@ -1,3 +1,3 @@
 export const completeWork = () => {
-  // node
+	// node
 };
---
+++ packages/reconciler/src/updateQueue.ts
@@ -0,0 +1,55 @@
+import { Action } from 'shared/ReactTypes';
+
+export interface Update<State> {
+  action: Action<State>;
+}
+
+export interface UpdateQueue<State> {
+  shared: {
+    pending: Update<State> | null;
+  };
+}
+
+export const createUpdate = <State>(action: Action<State>): Update<State> => {
+  return {
+    action
+  };
+};
+
+export const createUpdateQueue = <Action>() => {
+  return {
+    shared: {
+      pending: null
+    }
+  } as UpdateQueue<Action>;
+};
+
+export const enqueueUpdate = <Action>(
+  updateQueue: UpdateQueue<Action>,
+  update: Update<Action>
+) => {
+  updateQueue.shared.pending = update;
+};
+
+export const processUpdateQueue = <State>(
+  baseState: State,
+  pendingUpdate: Update<State> | null
+): { memoizeState: State } => {
+  const result: ReturnType<typeof processUpdateQueue<State>> = {
+    memoizeState: baseState
+  };
+
+  if (pendingUpdate !== null) {
+    const action = pendingUpdate.action;
+
+    if (action instanceof Function) {
+      // baseState 1 update (x) => 4x -> memoizeState 4
+      result.memoizeState = action(baseState);
+    } else {
+      // baseState 1 update 2 -> memoizeState 2
+      result.memoizeState = action;
+    }
+  }
+
+  return result;
+};
