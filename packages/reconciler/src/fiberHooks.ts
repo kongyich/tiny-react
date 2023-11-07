866f1edf026f93e8dbc921b4c8ed27ae81f9deab
884153724@qq.com <570588655@qq.com>
2023-11-07 23:59:05 +0800

    feat: add FunctionComponent

--- packages/reconciler/src/fiberHooks.ts
+++ packages/reconciler/src/fiberHooks.ts
@@ -1,9 +1,9 @@
 import { FiberNode } from './fiber';
 
 export function renderWithHooks(wip: FiberNode) {
-  const Component = wip.type;
-  const props = wip.pendingProps;
-  const children = Component(props);
+	const Component = wip.type;
+	const props = wip.pendingProps;
+	const children = Component(props);
 
-  return children;
+	return children;
 }
