6adce94b9121e4d6962fff5d0f2018c8419ba01b
884153724@qq.com <570588655@qq.com>
2023-11-13 23:12:53 +0800

    feat: add thired debugger

---
+++ demos/test-fc/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'dom/client';
+
+function App() {
+	return <div>hi, thired</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
---
+++ scripts/vite/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
+import { resolvePkgPath } from '../rollup/utils';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		replace({
+			__DEV__: true,
+			preventAssignment: true
+		})
+	],
+	resolve: {
+		alias: [
+			{
+				find: 'react',
+				replacement: resolvePkgPath('react')
+			},
+			{
+				find: 'dom',
+				replacement: resolvePkgPath('dom')
+			},
+			{
+				find: 'hostConfig',
+				replacement: path.resolve(resolvePkgPath('dom'), './src/hostConfig.ts')
+			}
+		]
+	}
+});
