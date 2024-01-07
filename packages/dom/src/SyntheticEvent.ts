982e1e84830d88f3c82635e15c73fe7d57b34037
884153724@qq.com <570588655@qq.com>
2024-01-07 11:58:17 +0800

    feat: add concurrent 1

--- packages/dom/src/SyntheticEvent.ts
+++ packages/dom/src/SyntheticEvent.ts
@@ -1,5 +1,11 @@
 import { Container } from 'hostConfig';
 import { Props } from 'shared/ReactTypes';
+import {
+	unstable_ImmediatePriority,
+	unstable_UserBlockingPriority,
+	unstable_NormalPriority,
+	unstable_runWithPriority
+} from 'scheduler';
 
 export const elementPropsKey = '__props';
 const validEventTypeList = ['click'];
@@ -81,7 +87,10 @@ function dispatchEvent(container: Container, eventType: string, e: Event) {
 function triggerEventFlow(paths: EventCallback[], se: SyntheticEvent) {
 	for (let i = 0; i < paths.length; i++) {
 		const callback = paths[i];
-		callback.call(null, se);
+
+		unstable_runWithPriority(eventTypeToSchdulerPriority(se.type), () => {
+			callback.call(null, se);
+		});
 
 		if (se.__stopPropagation) {
 			//阻止事件继续传播
@@ -132,4 +141,17 @@ function collectPaths(
 	}
 
 	return paths;
+}
+
+function eventTypeToSchdulerPriority(eventType: string) {
+	switch (eventType) {
+		case 'click':
+		case 'keydown':
+		case 'keyup':
+			return unstable_ImmediatePriority;
+		case 'scroll':
+			return unstable_UserBlockingPriority;
+		default:
+			return unstable_NormalPriority;
+	}
 }
--- packages/reconciler/src/fiberLanes.ts
+++ packages/reconciler/src/fiberLanes.ts
@@ -1,4 +1,11 @@
 import { FiberRootNode } from './fiber';
+import {
+	unstable_getCurrentPriorityLevel,
+	unstable_ImmediatePriority,
+	unstable_UserBlockingPriority,
+	unstable_NormalPriority,
+	unstable_IdlePriority
+} from 'scheduler';
 
 export type Lane = number;
 export type Lanes = number;
@@ -6,13 +13,23 @@ export type Lanes = number;
 export const SyncLane = 0b0001;
 export const NoLane = 0b0000;
 export const NoLanes = 0b0000;
+// 连续的输入
+export const InputContinuousLane = 0b0010;
+// 默认
+export const DefaultLane = 0b0100;
+// 空闲
+export const IdleLane = 0b1000;
 
 export function mergeLanes(laneA: Lane, laneB: Lane): Lanes {
 	return laneA | laneB;
 }
 
 export function requestUpdateLane() {
-	return SyncLane;
+	// 从上下文环境中获取优先级
+	const currentSchedulerPriority = unstable_getCurrentPriorityLevel();
+
+	const lane = schedulerPriorityToLane(currentSchedulerPriority);
+	return lane;
 }
 
 // 越小，优先级越高，始终返回最靠右的那一位
@@ -24,4 +41,34 @@ export function getHighestPriorityLane(lanes: Lanes): Lane {
 
 export function markRootfinished(root: FiberRootNode, lane: Lane) {
 	root.pendingLanes &= ~lane;
+}
+
+function lanesToSchedulerPriority(lans: Lanes) {
+	const lane = getHighestPriorityLane(lans);
+
+	if (lane === SyncLane) {
+		return unstable_ImmediatePriority;
+	}
+
+	if (lane === InputContinuousLane) {
+		return unstable_UserBlockingPriority;
+	}
+
+	if (lane === DefaultLane) {
+		return unstable_NormalPriority;
+	}
+	return unstable_IdlePriority;
+}
+
+function schedulerPriorityToLane(schedulerPriority: number) {
+	if (schedulerPriority === unstable_ImmediatePriority) {
+		return SyncLane;
+	}
+	if (schedulerPriority === unstable_UserBlockingPriority) {
+		return InputContinuousLane;
+	}
+	if (schedulerPriority === unstable_NormalPriority) {
+		return DefaultLane;
+	}
+	return NoLane;
 }
